7a46a4f8e87deebf7081ae1d4b5d29df2c3b8540
tanvir321110 <tanvirahmed321110@gmail.com>
2026-05-17 12:55:10 +0600

    timesheeet

--- controllers/timesheets.py
+++ controllers/timesheets.py
@@ -1,10 +1,11 @@
 from odoo import http
 from odoo.http import request
+from datetime import date
 
 class XsellencePortal(http.Controller):
-    @http.route('/timesheets',type='http',auth='public',website=True)
-    def timesheet_f (self,**kw):
-        return  request.render('xsellence_portal.timesheet_page',{
+    @http.route('/timesheets', type='http', auth='public', website=True)
+    def timesheet_f(self, **kw):
+        return request.render('xsellence_portal.timesheet_page', {
             'active_menu': 'timesheets',
             'breadcrumb': [
                 {'name': 'Dashboard', 'url': '/dashboard'},
@@ -37,3 +38,31 @@ class XsellencePortal(http.Controller):
             'breadcrumb': breadcrumb_data,
         })
 
+
+
+
+    # =============  Timesheet From Tasks  =============
+    @http.route('/tasks/add_timesheet', type='http', auth='public', website=True)
+    def add_timesheet_from_task(self, **kw):
+        task_id = int(kw.get('task_id', 0))
+        project_id = int(kw.get('project_id', 0))
+
+        selected_task = request.env['project.task'].sudo().browse(task_id)
+        selected_project = request.env['project.project'].sudo().browse(project_id)
+
+        projects = request.env['project.project'].sudo().search([])
+        tasks = request.env['project.task'].sudo().search([])
+
+        return request.render('xsellence_portal.add_timesheet_page', {
+            'active_menu': 'add_timesheet',
+            'selected_task': selected_task,
+            'tasks':tasks,
+            'projects' : projects,
+            'today': date.today(),
+            'selected_project': selected_project,  # ✅ fixed
+            'breadcrumb': [
+                {'name': 'Dashboard', 'url': '/dashboard'},
+                {'name': 'Tasks', 'url': '/tasks'},
+                {'name': 'Add Timesheet', 'url': False},
+            ]
+        })
