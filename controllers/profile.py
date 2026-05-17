566fd7c9d78c13b6475488fd9f0dd08d20a40421
Tanvir321110 <tanvirahmed321110@gmail.com>
2026-05-18 01:56:49 +0600

    profile page almos done

--- controllers/profile.py
+++ controllers/profile.py
@@ -3,14 +3,24 @@ from odoo.http import request
 
 
 class XsellencePortal(http.Controller):
-    @http.route('/profile', type='http', auth='public', website=True)
+    @http.route('/profile', type='http', auth='user', website=True)
     def profle_f(self, **kw):
+        user = request.env.user
+        employee = request.env['hr.employee'].sudo().search([('user_id', '=', user.id)], limit=1)
+
+        print(employee.name,employee.user_id)
+        print("User ID:", user.id)
+        print("User Login:", user.login)
+        print("Employee:", employee)
+        print("Employee Name:", employee.name)
+
         return request.render('xsellence_portal.profile_page', {
+            'user': user,
+            'employee': employee,
             'active_menu': 'profile',
         })
 
-
-class XsellencePortal(http.Controller):
+    # =========== For Edit Profile  =============
     @http.route('/edit_profile', type='http', auth='public', website=True)
     def edit_profile(self, **kw):
         return request.render('xsellence_portal.edit_profile_page', {
